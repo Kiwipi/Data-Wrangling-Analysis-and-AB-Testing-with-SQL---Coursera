caa3c18731a9f34e07145e0ae5c1859d9f81dba3
Kiki <88708914+Kiwipi@users.noreply.github.com>
2024-02-25 10:47:39 +0000

    Update and rename Module 1 Counting Users (Practice Exercise) to Module 1 Counting Users (Practice Exercise).sql

--- Module 1 Counting Users (Practice Exercise).sql
+++ Module 1 Counting Users (Practice Exercise).sql
@@ -3,45 +3,67 @@ added each day?“. Start by making sure you understand the columns in the table
 Exercise 2: WIthout worrying about deleted user or merged users, count the number of users
 added each day.
 
-    SELECT DATE(created_at) AS day, count(*) AS daily_added_users
-    FROM dsv1069.users
-    GROUP BY DATE(created_at)
-    ORDER BY DATE(created_at)
+    SELECT 
+        DATE(created_at) AS day, 
+        count(*) AS daily_added_users
+    FROM 
+        dsv1069.users
+    GROUP BY 
+        DATE(created_at)
+    ORDER BY 
+        DATE(created_at)
 
 Exercise 3: Consider the following query. Is this the right way to count merged or deleted
 users? If all of our users were deleted tomorrow what would the result look like?
 
-    SELECT DATE(created_at) AS day, count(*) AS daily_added_users
-    FROM dsv1069.users
-    WHERE deleted_at IS NULL 
-    AND (id <> parent_user_id OR parent_user_id IS NULL)
-    GROUP BY DATE(created_at)
-    ORDER BY DATE(created_at)
+    SELECT 
+        DATE(created_at) AS day, count(*) AS daily_added_users
+    FROM 
+        dsv1069.users
+    WHERE 
+        deleted_at IS NULL 
+    AND 
+        (id <> parent_user_id OR parent_user_id IS NULL)
+    GROUP BY 
+        DATE(created_at)
+    ORDER BY 
+        DATE(created_at)
 
 
 Exercise 4: Count the number of users deleted each day. Then count the number of users
 removed due to merging in a similar way
 
 --daily_deleted_users counting
-    SELECT date(deleted_at) AS day, count(*) AS daily_deleted_users
-    FROM dsv1069.users
-    WHERE deleted_at IS NOT NULL 
-    GROUP BY DATE(deleted_at)
-    ORDER BY DATE(deleted_at)
+    SELECT 
+        date(deleted_at) AS day, count(*) AS daily_deleted_users
+    FROM 
+        dsv1069.users
+    WHERE 
+        deleted_at IS NOT NULL 
+    GROUP BY 
+        DATE(deleted_at)
+    ORDER BY 
+        DATE(deleted_at)
 
 --daily_merged_user counting
-    SELECT DATE(merged_at) AS day, count(*) AS daily_merged_user
-    FROM dsv1069.users
-    WHERE merged_at IS NOT NULL 
-    GROUP BY DATE(merged_at)
-    ORDER BY DATE(merged_at)
+    SELECT 
+        DATE(merged_at) AS day, count(*) AS daily_merged_user
+    FROM 
+        dsv1069.users
+    WHERE 
+        merged_at IS NOT NULL 
+    GROUP BY 
+        DATE(merged_at)
+    ORDER BY    
+        DATE(merged_at)
     ) merged_users_table
 
 Exercise 5: Use the pieces you’ve built as subtables and create a table that has a column for
 the date, the number of users created, the number of users deleted and the number of users
 merged that day
 
-    SELECT new_users_table.day, new_users_table.daily_added_users, deleted_users_table.daily_deleted_users, merged_users_table.daily_merged_user
+    SELECT 
+        new_users_table.day, new_users_table.daily_added_users, deleted_users_table.daily_deleted_users, merged_users_table.daily_merged_user
     FROM
     (
         SELECT DATE(created_at) AS day, count(*) AS daily_added_users
@@ -59,7 +81,8 @@ merged that day
         GROUP BY DATE(deleted_at)
         ORDER BY DATE(deleted_at)
     ) deleted_users_table
-    ON new_users_table.day= deleted_users_table.day 
+    ON 
+        new_users_table.day= deleted_users_table.day 
     LEFT JOIN 
     (
         SELECT DATE(merged_at) AS day, count(*) AS daily_merged_user
@@ -69,7 +92,8 @@ merged that day
         GROUP BY DATE(merged_at)
         ORDER BY DATE(merged_at)
     ) merged_users_table
-    ON merged_users_table.day = new_users_table.day
+    ON 
+        merged_users_table.day = new_users_table.day
 
 Exercise 6: Refine your query from #5 to have informative column names and so that null
 columns return 0.
@@ -97,7 +121,8 @@ LEFT JOIN
     GROUP BY DATE(deleted_at)
     ORDER BY DATE(deleted_at)
 ) deleted_users_table
-ON new_users_table.day= deleted_users_table.day 
+ON 
+    new_users_table.day= deleted_users_table.day 
 LEFT JOIN 
 (
     SELECT DATE(merged_at) AS day, count(*) AS daily_merged_user
@@ -107,7 +132,8 @@ LEFT JOIN
     GROUP BY DATE(merged_at)
     ORDER BY DATE(merged_at)
 ) merged_users_table
-ON merged_users_table.day = new_users_table.day
+ON 
+    merged_users_table.day = new_users_table.day
 
 
 Exercise 7:
@@ -122,7 +148,7 @@ query, so that we won’t miss any dates.
     COALESCE(merged_users_table.daily_merged_user, 0) AS Merged_users,
     (COALESCE(new_users_table.daily_added_users, 0) - COALESCE(deleted_users_table.daily_deleted_users, 0) - COALESCE(merged_users_table.daily_merged_user, 0)) AS net_added_users
     FROM
-    dsv1069.dates_rollup
+        dsv1069.dates_rollup
     LEFT JOIN 
     (
         SELECT DATE(created_at) AS day, count(*) AS daily_added_users
@@ -132,7 +158,8 @@ query, so that we won’t miss any dates.
         GROUP BY DATE(created_at)
         ORDER BY DATE(created_at)
     ) new_users_table
-    ON dates_rollup.date = new_users_table.day
+    ON 
+        dates_rollup.date = new_users_table.day
     LEFT JOIN 
     (
         SELECT date(deleted_at) AS day, count(*)AS daily_deleted_users
@@ -141,7 +168,8 @@ query, so that we won’t miss any dates.
         GROUP BY DATE(deleted_at)
         ORDER BY DATE(deleted_at)
     ) deleted_users_table
-    ON new_users_table.day= deleted_users_table.day 
+    ON 
+        new_users_table.day= deleted_users_table.day 
     LEFT JOIN 
     (
         SELECT DATE(merged_at) AS day, count(*) AS daily_merged_user
@@ -151,4 +179,5 @@ query, so that we won’t miss any dates.
         GROUP BY DATE(merged_at)
         ORDER BY DATE(merged_at)
     ) merged_users_table
-    ON merged_users_table.day = dates_rollup.date
+    ON 
+        merged_users_table.day = dates_rollup.date
