37f1e0f5d1a1c727c9548a3d073e7f360919f7a2
Kiki <88708914+Kiwipi@users.noreply.github.com>
2024-02-25 10:48:48 +0000

    Update and rename Module 1 Flexible Data Formats (Practice Exercise) to Module 1 Flexible Data Formats (Practice Exercise).sql

--- Module 1 Flexible Data Formats (Practice Exercise).sql
+++ Module 1 Flexible Data Formats (Practice Exercise).sql
@@ -4,8 +4,10 @@ Goal: Write a query to format the view_item event into a table with the appropri
     SELECT
       event_id, event_time, user_id, platform, 
       (CASE WHEN parameter_name = 'item_id' THEN CAST(parameter_value  AS INT ) ELSE NULL END ) AS item_id
-    FROM dsv1069.events 
-    WHERE event_name ='view_item'
+    FROM 
+        dsv1069.events 
+    WHERE 
+        event_name ='view_item'
 
 
 
