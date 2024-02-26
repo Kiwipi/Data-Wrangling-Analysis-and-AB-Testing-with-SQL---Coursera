ae1d088ebe12b0dc06452057de811bb8ab2023f7
Kiki <88708914+Kiwipi@users.noreply.github.com>
2024-02-26 18:58:41 +0000

    Update Week 4 - Final Assessment.sql

--- Week 4 - Final Assessment.sql
+++ Week 4 - Final Assessment.sql
@@ -2,9 +2,6 @@ We are running an experiment at an item-level, which means all users who visit w
 
 
 
-5. Use the https://thumbtack.github.io/abba/demo/abba.html
- to compute the lifts in metrics and the p-values for the binary metrics ( 30 day order binary and 30 day view binary) using a interval 95% confidence. 
-
 6. Use Mode’s Report builder feature to write up the test. Your write-up should include a title, a graph for each of the two binary metrics you’ve calculated. The lift and p-value (from the AB test calculator) for each of the two metrics, and a complete sentence to interpret the significance of each of the results.
 
 
@@ -109,7 +106,104 @@ FROM
   GROUP BY 
     test_assignment
 
+ Results: 
+0	1130	399
+1	1068	381
+
+ OR 
+SELECT 
+test_assignment,
+COUNT (DISTINCT item_id) AS items_count,
+SUM(order_binary) AS orders_after_tested
+FROM
+(
+  SELECT 
+    item_id,
+    test_assignment,
+    MAX (CASE WHEN created_at > test_start_date AND 
+               DATE_PART('day', created_at - test_start_date) <=30
+               THEN 1 ELSE 0 END ) AS order_binary
+FROM 
+(
+    SELECT 
+      final_assignments.item_id, 
+      final_assignments.test_assignment, 
+      final_assignments.test_start_date,
+      orders.created_at 
+    FROM 
+      dsv1069.final_assignments
+    LEFT JOIN 
+      dsv1069.orders
+    ON 
+      orders.item_id = final_assignments.item_id
+    WHERE 
+      final_assignments.test_number = 'item_test_2'
+) all_item_2
+GROUP BY 
+    item_id,
+    test_assignment
+) order_binary
+GROUP BY 
+test_assignment
+
+ Results: 
+0	1130	341
+1	1068	319
+
+ -- Don't know why the result figures are not the same, anyone who can explain this?
+
 4. Use the final_assignments table to calculate the view binary, and average views for the 30 day window after the test assignment for item_test_2. (You may include the day the test started)
 -- Use this table to 
 -- compute view_binary for the 30 day window after the test_start_date
 -- for the test named item_test_2
+
+SELECT 
+  test_assignment,
+  COUNT(DISTINCT item_id) AS total_items,
+  SUM (view_binary ) AS total_views,
+  AVG(view_binary ) AS avg_views
+FROM 
+(
+  SELECT 
+      final_assignments.item_id,
+      final_assignments.test_assignment,
+      final_assignments.test_number,
+      MAX (CAST(CASE 
+          WHEN (view_item.event_time > final_assignments.test_start_date AND 
+                  DATE_PART('day', view_item.event_time - final_assignments.test_start_date) <=30 )
+          THEN 1 
+          ELSE 0 
+          END AS INT) )AS view_binary
+    FROM 
+      dsv1069.final_assignments 
+  LEFT JOIN 
+  (  
+      SELECT 
+        event_time,
+        (CASE WHEN parameter_name= 'item_id' THEN CAST (parameter_value AS INT) ELSE NULL END ) AS item_id
+      FROM 
+        dsv1069.events 
+      WHERE 
+        event_name = 'view_item'
+  ) view_item
+  ON 
+    view_item.item_id = final_assignments.item_id
+  WHERE
+    final_assignments.test_number = 'item_test_2'
+  GROUP BY 
+    final_assignments.item_id,
+    final_assignments.test_assignment,
+    final_assignments.test_number
+  ORDER BY 
+    final_assignments.item_id 
+    ) view_binary
+GROUP BY 
+  test_assignment
+
+ RESULTS:
+0	1130	918	0.8124
+1	1068	890	0.8333
+
+
+5. Use the https://thumbtack.github.io/abba/demo/abba.html
+ to compute the lifts in metrics and the p-values for the binary metrics ( 30 day order binary and 30 day view binary) using a interval 95% confidence. 
